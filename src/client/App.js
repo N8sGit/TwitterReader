a144633ad4e795bfcfeffadda1187e0092f52219
Nathan Anecone <nanecone@gmail.com>
2018-11-18 12:49:08 -0500

    update branch

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  state = { tweets: [] };
 
+  // tweets here will be processed without regards for template just an array of templated objects
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    // fetch('/api/getUsername')
+    //   .then(res => res.json())
+    //   .then(user => this.setState({ username: user.username }));
   }
 
   render() {
-    const { username } = this.state;
+    const { tweets } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        {tweets ? <h1>Welcome</h1> : <h1>Loading.. please wait!</h1>}
+        <div id="text-display">
+Tweets go here :
+          {tweets}
+        </div>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -5,4 +5,15 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/tweets', (req, res) => {
+  console.log(res);
+//   - user name
+// - user screen name (@whatever)
+// - user profile image
+// - tweet content
+// - number of retweets
+// - direct link to the tweet
+});
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
